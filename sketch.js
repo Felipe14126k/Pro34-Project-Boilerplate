15b4de08d3d75fe88574c0d890046c3346312d46
Felipe14126k <103604030+Felipe14126k@users.noreply.github.com>
2022-08-17 20:10:31 -0300

    commit
    commit

--- sketch.js
+++ sketch.js
@@ -1,25 +1,43 @@
+var canvas;
+var backgroundImage, car1_img, car2_img, track;
+var fuelImage, powerCoinImage, lifeImage;
+var obstacle1Image, obstacle2Image;
+var database, gameState;
+var form, player, playerCount;
+var allPlayers, car1, car2, fuels, powerCoins, obstacles;
+var cars = [];
 
-const Engine = Matter.Engine;
-const World = Matter.World;
-const Bodies = Matter.Bodies;
-const Body = Matter.Body;
-
-
-
+function preload() {
+  backgroundImage = loadImage("./assets/planodefundo.png");
+  car1_img = loadImage("../assets/car1.png");
+  car2_img = loadImage("../assets/car2.png");
+  track = loadImage("../assets/pista.png");
+  fuelImage = loadImage("./assets/fuel.png");
+  powerCoinImage = loadImage("./assets/goldCoin.png");
+  obstacle1Image = loadImage("./assets/obstacle1.png");
+  obstacle2Image = loadImage("./assets/obstacle2.png");
+  lifeImage = loadImage("./assets/life.png");
+}
 
 function setup() {
-  createCanvas(400,400);
-
-  engine = Engine.create();
-  world = engine.world;
-  
+  canvas = createCanvas(windowWidth, windowHeight);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
 }
 
+function draw() {
+  background(backgroundImage);
+  if (playerCount === 2) {
+    game.update(1);
+  }
 
-function draw() 
-{
-  background(51);
-  Engine.update(engine);
-  
+  if (gameState === 1) {
+    game.play();
+  }
 }
 
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
